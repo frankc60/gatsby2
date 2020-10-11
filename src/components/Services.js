15846f21a5d244e15872aafb645fdfc2e5b76f5f
frankc60 <frankc60@gmail.com>
2020-10-11 17:32:29 +0200

    services done

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,7 +2,23 @@ import React from "react"
 import Title from "./Title"
 import services from "../constants/services"
 const Services = () => {
-  return <h2>services component</h2>
+  return (
+    <section className="section bg-grey">
+      <Title title="services" />
+      <div className="section-center services-center">
+        {services.map(d => {
+          return (
+            <artcile key={d.id} className="service">
+              {d.icon}
+              <h4>{d.title}</h4>
+              <div className="underline"></div>
+              <p>{d.text}</p>
+            </artcile>
+          )
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default Services

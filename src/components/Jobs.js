9cee79346ac9a6f3852ebbfca98a70fe12ca6523
frankc60 <frankc60@gmail.com>
2020-10-12 22:35:15 +0200

    added strapi plugin, and strapi graphql with output working in jobs.js

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,8 +4,53 @@ import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        id
+        date
+        company
+        position
+        updated_at
+        description {
+          name
+          id
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+
+  const {
+    allStrapiJobs: { nodes },
+  } = data
+
+  return (
+    <article>
+      <h2>jobs component</h2>
+      {nodes.map(d => {
+        return (
+          <article key={d.id}>
+            {d.company} - {d.date} - {d.position}
+            <div>
+              {d.description.map(x => {
+                return (
+                  <p>
+                    {x.id} - {x.name}
+                  </p>
+                )
+              })}
+            </div>
+          </article>
+        )
+      })}
+      <p>{JSON.stringify(data)}</p>
+    </article>
+  )
 }
 
 export default Jobs
